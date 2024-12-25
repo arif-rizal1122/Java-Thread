32ee29caf1f1f20862001f1ebae0d06fa5db7ac6
arif-rizal1122 <rizallarif32@gmail.com>
2024-12-25 18:52:57 +0800

    first commit

---
+++ src/test/java/arif/rizal/now/BelajarJavaThreadApplicationTests.java
@@ -0,0 +1,13 @@
+package arif.rizal.now;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarJavaThreadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
